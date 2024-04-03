d626f6ab8291a857f4f5ed0c4544d7600e24e0c9
katrombs <105538604+katrombs@users.noreply.github.com>
2024-04-03 20:41:28 +0800

    Update asmfunc.asm with loop inside asm kernel

--- asmfunc.asm
+++ asmfunc.asm
@@ -9,13 +9,36 @@
 
 ; assembly part using x86-64
 
+section .data
+zero dq 0.0
+
 section .text
 bits 64
-default rel ; to handle address relocation
+default rel
 
 global dotprod
 
 dotprod:
-	vmulsd xmm3, xmm0, xmm1
-	vaddsd xmm0, xmm3, xmm2
+	push rbp
+	mov rbp, rsp
+	; rcx -> vector a ; rdx -> vector b ; r8 -> size
+	xor rax, rax			; counter
+
+	movsd xmm0, [zero]
+
+DOT_PROD:
+	cmp rax, r8
+	jge END
+
+	movsd xmm5, qword[rcx + rax*8]
+	movsd xmm6, qword[rdx + rax*8]
+
+	vmulsd xmm4, xmm5, xmm6
+	addsd xmm0, xmm4
+
+	inc rax
+	jmp DOT_PROD
+	
+END:
+	pop rbp
 	ret
