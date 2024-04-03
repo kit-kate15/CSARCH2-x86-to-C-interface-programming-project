dfffee61386b067a8e331a7af8a9734fdb22503d
katrombs <105538604+katrombs@users.noreply.github.com>
2024-04-03 21:51:43 +0800

    final main.c

--- x86-to-C-programming-project/main.c
+++ x86-to-C-programming-project/main.c
@@ -1,6 +1,13 @@
+/*
+* LBYARCH XX22
+* Rojo, Kate Lynn
+* Romblon, Kathleen Mae
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <time.h>
 
 /*
     Dot Product Example
@@ -10,38 +17,91 @@
     r = ( 2 * 3) + (5 * 2) + (4 * 1) = 6 + 10 + 4 = 20
 */
 
-float dot_product(float v1[], float v2[], int size) {
-    float result = 0;
+extern double dotprod(double *vA, double *vB, int size);
+
+double dot_product(double vA[], double vB[], int size) {
+    double result = 0;
     for (int i = 0; i < size; i++) {
-        result += v1[i] * v2[i];
+        result += vA[i] * vB[i];
     }
     return result;
 }
 
+double randFrom(double min, double max) {
+    double range = (max - min);
+    double div = RAND_MAX / range;
+    return min + (rand() / div);
+}
+
 int main(void) {
     int size;
-    float result;
+    double result;
+    double dot_prod = 0;
+    clock_t start_c, start_asm, end_c, end_asm;
+    double cpu_time_c, cpu_time_asm;
+    double avg_c, avg_asm;
 
+    srand(time(NULL));
+
+    // C kernel
     printf("Enter the size or length of the vectors: ");
     scanf_s("%d", &size);
 
-    float* v1 = (float*)malloc(size * sizeof(float));
-    float* v2 = (float*)malloc(size * sizeof(float));
+    double* vA = (double*)malloc(size * sizeof(double));
+    double* vB = (double*)malloc(size * sizeof(double));
 
-    printf("Enter the elements of the first vector: ");
+    //printf("Enter the elements of the first vector: ");
+    //printf("Elements of the first vector: ");
     for (int i = 0; i < size; i++) {
-        scanf_s("%f", &v1[i]);
+        // scanf_s("%lf", &vA[i]);
+        vA[i] = randFrom(1.0, 10.0);
+        //printf("%.2lf ", vA[i]);
     }
-    printf("Enter the elements of the second vector: ");
+    printf("\n");
+
+    //printf("Enter the elements of the second vector: ");
+    //printf("Elements of the second vector: ");
     for (int i = 0; i < size; i++) {
-        scanf_s("%f", &v2[i]);
+        //scanf_s("%lf", &vB[i]);
+        vB[i] = randFrom(1.0, 10.0);
+        //printf("%.2lf ", vB[i]);
     }
-    result = dot_product(v1, v2, size);
-    printf("Dot Product: %.f\n", result);
+    printf("\n");
+
+    // C kernel
+    avg_c = 0;
+    for (int i = 0; i < 30; i++) {
+        start_c = clock();
+        result = dot_product(vA, vB, size);
+        end_c = clock();
+        cpu_time_c = ((double)end_c - start_c) / CLOCKS_PER_SEC;
+        avg_c += cpu_time_c;
+
+    }
+    avg_c /= 30;
+
+    printf("Dot Product from C: %.2lf\n", result);
+    printf("Average time taken (after 30 runs) to run C kernel: %lf\n", avg_c);
+
+    // asm kernel
+    avg_asm = 0;
+    int loop = 0;
+    while (loop < 30) {
+        start_asm = clock();
+        dot_prod = dotprod(vA, vB, size);
+        end_asm = clock();
+        cpu_time_asm = ((double)end_asm - start_asm) / CLOCKS_PER_SEC;
+        avg_asm += cpu_time_asm;
+        loop++;
+    }
+    avg_asm /= 30;
+
+    printf("Dot Product from asm: %.2lf\n", dot_prod);
+    printf("Average time taken (after 30 runs) to run asm kernel: %lf\n", avg_asm);
 
     //Deallocate memory
-    free(v1);
-    free(v2);
+    free(vA);
+    free(vB);
 
     return 0;
 }
